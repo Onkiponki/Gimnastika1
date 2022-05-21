998d59d73dd1ec772cde95e404c6c315b2c54e0c
Onkiponki <pavle.georgijev@gmail.com>
2022-05-21 20:46:28 +0200

    Tralala

--- Menu.Designer.cs
+++ Menu.Designer.cs
@@ -28,12 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.podaciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.osobeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.takmicenjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.godineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.podaciToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // podaciToolStripMenuItem
+            // 
+            this.podaciToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.osobeToolStripMenuItem,
+            this.takmicenjaToolStripMenuItem,
+            this.godineToolStripMenuItem});
+            this.podaciToolStripMenuItem.Name = "podaciToolStripMenuItem";
+            this.podaciToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.podaciToolStripMenuItem.Text = "Podaci";
+            // 
+            // osobeToolStripMenuItem
+            // 
+            this.osobeToolStripMenuItem.Name = "osobeToolStripMenuItem";
+            this.osobeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.osobeToolStripMenuItem.Text = "Osobe";
+            // 
+            // takmicenjaToolStripMenuItem
+            // 
+            this.takmicenjaToolStripMenuItem.Name = "takmicenjaToolStripMenuItem";
+            this.takmicenjaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.takmicenjaToolStripMenuItem.Text = "Takmicenja";
+            // 
+            // godineToolStripMenuItem
+            // 
+            this.godineToolStripMenuItem.Name = "godineToolStripMenuItem";
+            this.godineToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.godineToolStripMenuItem.Text = "Godine";
+            // 
+            // Menu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Menu";
             this.Text = "Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem podaciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem osobeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem takmicenjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem godineToolStripMenuItem;
     }
 }
